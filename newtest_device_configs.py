ae051e7cf173cea0c3efcabc748d704ac1f3f446
moonstomper <ogiatienza@gmail.com>
2017-08-18 02:10:42 -0700

    my first upload

---
+++ newtest_device_configs.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import unittest
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+requests.packages.urllib3.disable_warnings()
+
+class TestDeviceConfiguration(unittest.TestCase):
+
+    def test_npdesi_snmp_ro(self):
+        """Validate npdesi is a configured RO string
+        """
+
+        auth = HTTPBasicAuth('cisco', 'cisco')
+        headers = { 'Accept': 'application/vnd.yang.data+json'}
+        url = 'http://csr1kv/restconf/api/config/native/snmp-server?deep'
+        response = requests.get(url, verify=False, headers=headers, auth=auth)
+
+        parse = json.loads(response.text)
+        community_list = parse['ned:snmp-server']['community']
+        expected = {'name': 'npdesi', 'RO': [None]}
+        is_there = expected in community_list
+        self.assertTrue(is_there)
+
+
+    def test_cisco_snmp_ro(self):
+        """Validate cisco is a configured RO string
+        """
+
+        auth = HTTPBasicAuth('cisco', 'cisco')
+        headers = { 'Accept': 'application/vnd.yang.data+json'}
+        url = 'http://csr1kv/restconf/api/config/native/snmp-server?deep'
+        response = requests.get(url, verify=False, headers=headers, auth=auth)
+
+        parse = json.loads(response.text)
+        community_list = parse['ned:snmp-server']['community']
+        expected = {'name': 'cisco', 'RO': [None]}
+        is_there = expected in community_list
+        self.assertTrue(is_there)
+
+    def test_cisco_secure_snmp_rw(self):
+        """Validate cisco_secure is a configured RW string
+        """
+
+        auth = HTTPBasicAuth('cisco', 'cisco')
+        headers = { 'Accept': 'application/vnd.yang.data+json'}
+        url = 'http://csr1kv/restconf/api/config/native/snmp-server?deep'
+        response = requests.get(url, verify=False, headers=headers, auth=auth)
+
+        parse = json.loads(response.text)
+        community_list = parse['ned:snmp-server']['community']
+        expected = {'name': 'cisco_secure', 'RW': [None]}
+        is_there = expected in community_list
+        self.assertTrue(is_there)
+
+
+    def test_user_devops(self):
+
+        auth = HTTPBasicAuth('cisco', 'cisco')
+        headers = { 'Accept': 'application/vnd.yang.collection+json'}
+        url = 'http://csr1kv/restconf/api/config/native/username'
+        response = requests.get(url, verify=False, headers=headers, auth=auth)
+	print response.text
+
+        parse = json.loads(response.text)
+        user_list = parse['collection']['ned:username']
+        expected = {'name': 'devops'}
+        is_there = expected in user_list 
+        self.assertTrue(is_there)
+
+    def test_user_super_user(self):
+
+        auth = HTTPBasicAuth('cisco', 'cisco')
+        headers = { 'Accept': 'application/vnd.yang.collection+json'}
+        url = 'http://csr1kv/restconf/api/config/native/username'
+        response = requests.get(url, verify=False, headers=headers, auth=auth)
+	print response.text
+        parse = json.loads(response.text)
+        user_list = parse['collection']['ned:username']
+        expected = {'name': 'super_user'}
+        is_there = expected in user_list
+        self.assertTrue(is_there)
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+              
